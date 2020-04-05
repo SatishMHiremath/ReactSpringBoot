49c19d01eba498cbe8bce5bae6a2d22a9bb68244
Satish M Hiremath <smhirema@lowes.com>
2020-04-06 02:35:02 +0530

    Webpack Changes

---
+++ ReactSpringBoot/SpringBootReactCrud/src/main/resources/static/precache-manifest.654774c1fa9f5bf917f13b00797296ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faea426938a607f532642d0e6ce2447e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ba662095b17efa9827d",
+    "url": "/static/css/main.089419b2.chunk.css"
+  },
+  {
+    "revision": "cbcd302b256613f00bbc",
+    "url": "/static/js/2.e8fe2d1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8fe2d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba662095b17efa9827d",
+    "url": "/static/js/main.493a94ec.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
--- ReactSpringBoot/SpringBootReactCrud/src/main/resources/static/service-worker.js
+++ ReactSpringBoot/SpringBootReactCrud/src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efdc78ece8775ce03fc722e30de8d2c0.js"
+  "/precache-manifest.654774c1fa9f5bf917f13b00797296ea.js"
 );
 
 self.addEventListener('message', (event) => {
